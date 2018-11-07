a33130d4299199cbe9c000f7bc764de42222d4c6
Andy Autida <risejandiautida@gmail.com>
2018-11-08 00:17:09 +0800

    refactor rest api

--- index.js
+++ index.js
@@ -1,89 +1,49 @@
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 const express = require('express')
-const uuid = require('uuid/v4')
-const AWS = require('aws-sdk')
-const app = express()
 
-const isOffline = process.env.IS_OFFLINE === 'true'
-const dynamoOpts = isOffline ? { endpoint: 'http://localhost:8000' } : {}
-const dynamoDb = new AWS.DynamoDB.DocumentClient(dynamoOpts)
-const { FARMERS_TABLE, LOCATIONS_TABLE } = process.env
+const locations = require('./lib/locations')
+const farmers = require('./lib/farmers')
+
+const app = express()
 
 app.use(bodyParser.json({ strict: false }))
 
 app.get('/farmers/:farmerId', async (req, res) => {
-  const params = {
-    TableName: FARMERS_TABLE,
-    Key: { farmerId: req.params.farmerId }
-  }
-
   try {
-    const { Item } = await dynamoDb.get(params).promise()
-    const { farmerId, name, phone, subscribed } = Item || {}
-    const { status, body } = (Item)
-      ? { status: 200, body: { farmerId, name, phone } }
-      : { status: 404, body: { error: 'Farmer not found' } }
-    res.status(status).json(body)
+    const farmer = await farmers.get(req.params.farmerId)
+    res.status(200).json(farmer)
   } catch (e) {
-    console.error(e)
-    res.status(400).json({ error: 'Could not get farmer' })
+    const status = e.message === 'Farmer not found' ? 404 : 500
+    res.status(status).json({ error: e.message })
   }
 })
 
 app.post('/farmers', async (req, res) => {
-  const farmerId = uuid()
-  const { name, phone } = req.body
-
-  const params = {
-    TableName: FARMERS_TABLE,
-    Item: { farmerId, name, phone, subscribed: [] }
-  }
-
   try {
-    await dynamoDb.put(params).promise()
-    res.json(params.Item)
+    const farmer = await farmers.create(req.body)
+    res.status(400).json(farmer)
   } catch (e) {
-    console.error(e)
-    res.status(400).json({ error: 'Could not create farmer' })
+    res.status(500).json({ error: e.message })
   }
 })
 
 app.get('/locations/:locationId', async (req, res) => {
-  const params = {
-    TableName: LOCATIONS_TABLE,
-    Key: { locationId: req.params.locationId }
-  }
-
   try {
-    const { Item } = await dynamoDb.get(params).promise()
-    const { locationId, barangay, municipality, province, longitude, latitude, subscribers } =
-      Item || {}
-    const { status, body } = (Item)
-      ? { status: 200, body: { locationId, barangay, municipality, province, longitude, latitude } }
-      : { status: 404, body: { error: 'Location not found' } }
-    res.status(status).json(body)
+    const location = await locations.get(req.params.locationId )
+    res.status(200).json(location)
   } catch (e) {
-    console.error(e)
-    res.status(400).json({ error: 'Could not get location' })
+    const status = e.message === 'Location not found' ? 404 : 500
+    res.status(status).json({ error: e.message })
   }
 })
 
 app.post('/locations', async (req, res) => {
-  const locationId = uuid()
-  const { barangay, municipality, province, longitude, latitude } = req.body
-
-  const params = {
-    TableName: LOCATIONS_TABLE,
-    Item: { locationId, barangay, municipality, province, longitude, latitude, subscribers: [] }
-  }
-
   try {
-    await dynamoDb.put(params).promise()
-    res.json(params.Item)
+    const location = await locations.create(req.body)
+    res.status(200).json(location)
   } catch (e) {
-    console.error(e)
-    res.status(400).json({ error: 'Could not create location' })
+    res.status(500).json({ error: e.message })
   }
 })
 
