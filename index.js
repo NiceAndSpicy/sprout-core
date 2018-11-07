170e9822609373c14c832210f9ce1c7546d59b85
Andy Autida <risejandiautida@gmail.com>
2018-11-07 23:35:19 +0800

    add api for locations

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const isOffline = process.env.IS_OFFLINE === 'true'
 const dynamoOpts = isOffline ? { endpoint: 'http://localhost:8000' } : {}
 const dynamoDb = new AWS.DynamoDB.DocumentClient(dynamoOpts)
-const { FARMERS_TABLE } = process.env
+const { FARMERS_TABLE, LOCATIONS_TABLE } = process.env
 
 app.use(bodyParser.json({ strict: false }))
 
@@ -20,7 +20,7 @@ app.get('/farmers/:farmerId', async (req, res) => {
 
   try {
     const { Item } = await dynamoDb.get(params).promise()
-    const { farmerId, name, phone } = Item || {}
+    const { farmerId, name, phone, subscribed } = Item || {}
     const { status, body } = (Item)
       ? { status: 200, body: { farmerId, name, phone } }
       : { status: 404, body: { error: 'Farmer not found' } }
@@ -37,7 +37,7 @@ app.post('/farmers', async (req, res) => {
 
   const params = {
     TableName: FARMERS_TABLE,
-    Item: { farmerId, name, phone }
+    Item: { farmerId, name, phone, subscribed: [] }
   }
 
   try {
@@ -46,6 +46,44 @@ app.post('/farmers', async (req, res) => {
   } catch (e) {
     console.error(e)
     res.status(400).json({ error: 'Could not create farmer' })
+  }
+})
+
+app.get('/locations/:locationId', async (req, res) => {
+  const params = {
+    TableName: LOCATIONS_TABLE,
+    Key: { locationId: req.params.locationId }
+  }
+
+  try {
+    const { Item } = await dynamoDb.get(params).promise()
+    const { locationId, barangay, municipality, province, longitude, latitude, subscribers } =
+      Item || {}
+    const { status, body } = (Item)
+      ? { status: 200, body: { locationId, barangay, municipality, province, longitude, latitude } }
+      : { status: 404, body: { error: 'Location not found' } }
+    res.status(status).json(body)
+  } catch (e) {
+    console.error(e)
+    res.status(400).json({ error: 'Could not get location' })
+  }
+})
+
+app.post('/locations', async (req, res) => {
+  const locationId = uuid()
+  const { barangay, municipality, province, longitude, latitude } = req.body
+
+  const params = {
+    TableName: LOCATIONS_TABLE,
+    Item: { locationId, barangay, municipality, province, longitude, latitude, subscribers: [] }
+  }
+
+  try {
+    await dynamoDb.put(params).promise()
+    res.json(params.Item)
+  } catch (e) {
+    console.error(e)
+    res.status(400).json({ error: 'Could not create location' })
   }
 })
 
